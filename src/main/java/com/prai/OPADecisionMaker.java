46fe6ac6bbafbd08f63c23335adce2ed5575ec98
epraria <prashanna.rai@ericsson.com>
2023-05-24 14:18:31 +0200

    optimized bundle object

--- src/main/java/com/prai/OPADecisionMaker.java
+++ src/main/java/com/prai/OPADecisionMaker.java
@@ -25,16 +25,22 @@ public class OPADecisionMaker {
     private static String coarsedGrainedIRFile = "/opt/ericsson/hds/resources/coarse-grained-policies-plan.json";
     private static String fineGrainedIRFile = "/opt/ericsson/hds/resources/fine-grained-policies-plan.json";
     private static String coarsedGrainedentrypoint = "authz/redfish/v1/policy/allow";
-    private static String fineGrainedIRFileentrypoint = "authz/redfish/v1/fine/policy/batch_allow";
+    private static String fineGrainedentrypoint = "authz/redfish/v1/fine/policy/batch_allow";
 
-    public static Plan coarsedGraindePlan;
-    public static Plan fineGraindePlan;
+    private static Plan coarsedGraindePlan;
+    private static Plan fineGraindePlan;
+    private static Bundle coarsedGrainedWASMBundle;
+
+    private static Bundle fineGrainedWASMBundle;
 
     public static void init() throws IOException {
         var file = new File(coarsedGrainedIRFile);
         coarsedGraindePlan = Jarl.builder(file).build().getPlan(coarsedGrainedentrypoint);
         var file1 = new File(fineGrainedIRFile);
-        fineGraindePlan = Jarl.builder(file).build().getPlan(fineGrainedIRFileentrypoint);
+        fineGraindePlan = Jarl.builder(file).build().getPlan(fineGrainedentrypoint);
+        coarsedGrainedWASMBundle = BundleUtil.extractBundle("/repo/policy/tmp/coarsed-bundle.tar.gz");
+        fineGrainedWASMBundle=  BundleUtil.extractBundle("/repo/policy/unoptimizedWASM/finebundle.tar.gz");
+
     }
 
     public static boolean isAllowedJarl(String uri, String method, List<String> roles) {
@@ -79,12 +85,12 @@ public class OPADecisionMaker {
         ObjectMapper objectMapper = new ObjectMapper();
         WASMSingleInput input = new WASMSingleInput(method, uri, roles);
 
-        Bundle bundle = BundleUtil.extractBundle("/repo/policy/tmp/coarsed-bundle.tar.gz");
         try (
-                OPAModule om = new OPAModule(bundle);
+                OPAModule om = new OPAModule(coarsedGrainedWASMBundle);
         ) {
             String inputJson = objectMapper.writeValueAsString(input);
-            String output = om.evaluate(inputJson, "authz/redfish/v1/policy/allow");
+            String output = om.evaluate(inputJson, coarsedGrainedentrypoint);
+
             var rawoutput = objectMapper.readValue(output, WASMOutput[].class);
             System.out.println(output);
             opaDecision = rawoutput[0].isResult();
@@ -96,12 +102,11 @@ public class OPADecisionMaker {
         List<String> allowedURIS = Collections.EMPTY_LIST;
         ObjectMapper objectMapper = new ObjectMapper();
         WASMBulkInput input = new WASMBulkInput(methods, uris, roles);
-        Bundle bundle = BundleUtil.extractBundle("/repo/policy/unoptimizedWASM/finebundle.tar.gz");
         try (
-                OPAModule om = new OPAModule(bundle);
+                OPAModule om = new OPAModule(fineGrainedWASMBundle);
         ) {
             String inputJson = objectMapper.writeValueAsString(input);
-            String output = om.evaluate(inputJson, "authz/redfish/v1/fine/policy/batch_allow");
+            String output = om.evaluate(inputJson, fineGrainedentrypoint);
             var rawoutput = objectMapper.readValue(output, WASMBulkOutput[].class);
             System.out.println(rawoutput[0].getResult());
             allowedURIS = rawoutput[0].getResult();
